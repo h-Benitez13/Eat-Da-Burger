c0a68518dfcdb19c05990d9556552ddfc686f108
h-Benitez13 <hbenitez3711@gmail.com>
2019-02-26 20:14:30 -0500

    initial commit

---
+++ burger/db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name varchar(225),
+    devoured BOOLEAN NOT NULL default 0,
+    PRIMARY KEY(id)
+
+);
---
+++ burger/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", "false");
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", "false");
+INSERT INTO burgers (burger_name, devoured) VALUES ("grilled cheese", "false");
+INSERT INTO burgers (burger_name, devoured) VALUES ("taco", "false");
